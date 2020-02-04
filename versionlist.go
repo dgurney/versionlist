360e6d6ad8337e31e80da38dec2f0a62db3dcce0
Daniel Gurney <dgurney99@gmail.com>
2020-02-05 00:58:24 +0200

    Initial version of versionlist

---
+++ versionlist.go
@@ -0,0 +1,128 @@
+package main
+
+/*
+Copyright (c) 2020 Daniel Gurney
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+
+	"github.com/gonutz/w32"
+)
+
+const version = "0.0.1"
+
+func getVersions(dir string) map[string]string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	versions := make(map[string]string, 0)
+	for _, file := range files {
+		name := strings.ToLower(file.Name())
+		// We are only interested in dll, exe, mui or sys files. We silently ignore any problematic files.
+		if filepath.Ext(name) == ".exe" || filepath.Ext(name) == ".dll" || filepath.Ext(name) == ".sys" || filepath.Ext(name) == ".mui" {
+			size := w32.GetFileVersionInfoSize(dir + `\` + name)
+			if size == 0 {
+				continue
+			}
+
+			info := make([]byte, size)
+			w32.GetFileVersionInfo(dir+`\`+name, info)
+
+			translation, success := w32.VerQueryValueTranslations(info)
+			if !success {
+				continue
+			}
+
+			version, success := w32.VerQueryValueString(info, translation[0], "FileVersion")
+			if !success {
+				continue
+			}
+			versions[name] = version
+		}
+	}
+
+	return versions
+}
+
+func main() {
+	dir := flag.String("d", ".", "Directory to read.")
+	name := flag.Bool("n", false, "Show names in output.")
+	win := flag.Bool("w", false, "Only show Windows build tags that could theoretically be full Windows builds.")
+	v := flag.Bool("v", false, "Show version and exit.")
+	flag.Parse()
+	if *v {
+		fmt.Printf("versionlist v%s by Daniel Gurney.\n", version)
+		return
+	}
+	vers := getVersions(*dir)
+
+	switch {
+	default:
+		rawVersions := make([]string, 0)
+		for _, v := range vers {
+			rawVersions = append(rawVersions, v)
+		}
+
+		encountered := map[string]bool{}
+		versions := make([]string, 0)
+		for v := range rawVersions {
+			switch {
+			case encountered[rawVersions[v]]:
+				// No dupes please
+			default:
+				encountered[rawVersions[v]] = true
+				versions = append(versions, rawVersions[v])
+			}
+		}
+
+		sort.Strings(versions)
+		for _, version := range versions {
+			switch {
+			default:
+				fmt.Println(version)
+			case *win:
+				r, _ := regexp.Compile(`^(5|6|10).[0-5]{1}.[^0][0-9]{3,4}.[\d]{1,5}[[:space:]]\([a-zA-Z\S]+.[\d]{6}-`)
+				if r.MatchString(version) {
+					fmt.Println(version)
+				}
+			}
+		}
+	case *name:
+		names := make([]string, 0)
+		for n := range vers {
+			names = append(names, n)
+		}
+
+		sort.Strings(names)
+		for _, name := range names {
+			fmt.Printf("%s: %s\n", name, vers[name])
+		}
+	}
+}
